6dd70388b4229e27851572162cd925c8a115bf66
KathyVira <katy2002@gmail.com>
2022-12-26 14:37:27 +0200

    git hup added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,12 @@ function Footer() {
         <div className="footer-copyright">
           <div className="container">
             © {new Date().getFullYear()} Copyright Text
-            <a className="grey-text text-lighten-4 right" href="#!">
+            <a
+              className="grey-text text-lighten-4 right"
+              href="https://github.com/KathyVira/react-shop"
+              targer="_blank"
+              rel="noreferrer"
+            >
               Repo
             </a>
           </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,18 @@ function Header() {
     <>
       <nav className="purple darken-4">
         <div className="nav-wrapper ">
-          <a href="#" className="brand-logo">
+          <a href="/" className="brand-logo">
             React Shop
           </a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li>
-              <a href="https://kathyvira.com">Repo</a>
+              <a
+                href="https://github.com/KathyVira/react-shop"
+                targer="_blank"
+                rel="noreferrer"
+              >
+                Repo
+              </a>
             </li>
           </ul>
         </div>
